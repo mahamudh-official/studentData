c36373f26db2c685fe8a9787e01959441146c3ff
unknown <mymahamud.ha@gmail.com>
2026-06-07 12:37:19 +0600

    Done

---
+++ flutter_practice_1/lib/main.dart
@@ -0,0 +1,319 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: student());
+  }
+}
+
+class student extends StatelessWidget {
+  const student({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student Profile'),
+        leading: Icon(Icons.arrow_back),
+        actions: [
+          Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.info)),
+        ],
+      ),
+      body: Home(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 2,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cast_for_education),
+            label: 'Academics',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              width: double.maxFinite,
+            
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 24, 0, 12),
+                      child: CircleAvatar(
+                        radius: 70,
+                        foregroundColor: Colors.white60,
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                          size: 70,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Md Mahamud Hasan",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey.shade100,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+
+                      padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                      child: Text(
+                        'Roll: 784303',
+                        style: TextStyle(fontSize: 12, color: Colors.black),
+                      ),
+                    ),
+                    Text('Telecommunication Technology'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(36),
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.lightBlueAccent.shade100,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              
+                              children: [
+                                Icon(Icons.calendar_month,size: 35,),
+                                Spacer(),
+                                Text('On Track', style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold
+                                ),)
+                              ],
+                            ),
+                            SizedBox(height: 10,),
+                            Text('ATTENDENCE', style: TextStyle(
+                              fontSize: 14
+                            ),),
+                            SizedBox(height: 4,),
+                            Text('85%', style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue
+                            ),),
+                            SizedBox(height: 10,),
+                            LinearProgressIndicator(
+                              value: 0.85,
+                              color: Colors.green,
+                            )
+                        
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(36),
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.lightBlueAccent.shade100,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              
+                              children: [
+                                Icon(Icons.star,size: 35,),
+                                Spacer(),
+                                Text('Top 10%', style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold
+                                ),)
+                              ],
+                            ),
+                            SizedBox(height: 10,),
+                            Text('CGPA', style: TextStyle(
+                              fontSize: 14
+                            ),),
+                            SizedBox(height: 4,),
+                            Text('3.75', style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue
+                            ),),
+                            SizedBox(height: 8,),
+                            Text('Last Updated: Semester 5', style: TextStyle(
+                              fontSize: 10
+                            ),)
+                        
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('PERSONAL DETAILS', style: TextStyle(fontSize: 14)),
+                SizedBox(height: 15),
+
+                detailBar(
+                  title: 'Email Address',
+                  description: 'mahamud@gmail.com',
+                  icon: Icons.message,
+                ),
+                SizedBox(height: 10),
+
+                detailBar(
+                  title: 'Phone Number',
+                  description: '018 XXXX XXXX',
+                  icon: Icons.call,
+                ),
+                SizedBox(height: 10),
+
+                detailBar(
+                  title: 'Home Adress',
+                  description: 'Noakhali, Bangladesh',
+                  icon: Icons.location_city,
+                ),
+                SizedBox(height: 15),
+              ],
+            ),
+            SizedBox(
+              width: double.maxFinite,
+              child: buttons(title: 'Edit Profile', icon: Icons.edit, bgcolor: Colors.blueAccent, fgcolor: Colors.white,)),
+            SizedBox(height: 10,),
+            SizedBox(
+              width: double.maxFinite,
+              child: buttons(title: 'Logout', icon: Icons.logout, bgcolor: Colors.white, fgcolor: Colors.black,)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class detailBar extends StatelessWidget {
+  const detailBar({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.icon,
+  });
+
+  final String title;
+  final String description;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(24),
+      ),
+      height: 80,
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Row(
+          children: [
+            Icon(icon),
+            SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: TextStyle(fontSize: 12)),
+                SizedBox(height: 2),
+                Text(description),
+              ],
+            ),
+            Spacer(),
+            Icon(Icons.arrow_forward),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class buttons extends StatelessWidget {
+  const buttons({super.key, required this.title, required this.icon, required this.bgcolor, required this.fgcolor,});
+
+  final String title;
+  final IconData icon;
+  final Color bgcolor;
+  final Color fgcolor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {},
+      label: Text(title),
+      icon: Icon(icon),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(12)),
+        backgroundColor: bgcolor,
+        foregroundColor: fgcolor
+      ),
+    );
+  }
+}
