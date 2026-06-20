8781729218492f0e3907f8a83d7538a86a8494d2
unknown <mymahamud.ha@gmail.com>
2026-06-20 21:02:08 +0600

    7 done

---
+++ practice_7/lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffe50914);
+  static const Color secondaryColor = Color(0xffffffff);
+  static const Color tertiaryColor = Color(0xff0072d7);
+  static const Color neatralColor = Color(0xff000000);
+}
---
+++ practice_7/lib/main.dart
@@ -0,0 +1,393 @@
+import 'package:flutter/material.dart';
+import 'package:practice_7/core/theme/app_colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: NetFlixHome());
+  }
+}
+
+class NetFlixHome extends StatefulWidget {
+  const NetFlixHome({super.key});
+
+  @override
+  State<NetFlixHome> createState() => _NetFlixHomeState();
+}
+
+class _NetFlixHomeState extends State<NetFlixHome> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        selectedItemColor: AppColors.primaryColor,
+        backgroundColor: AppColors.neatralColor,
+        unselectedItemColor: Colors.white,
+        onTap: (value) => setState(() {
+          currentIndex = value;
+        }),
+
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: AppColors.neatralColor,
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gamepad),
+            label: 'Home',
+            backgroundColor: AppColors.neatralColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_library_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.percent_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.download_outlined),
+            label: 'Home',
+          ),
+        ],
+      ),
+      backgroundColor: AppColors.neatralColor,
+      appBar: AppBar(
+        title: const Text(
+          'NETFLIX',
+          style: TextStyle(
+            color: AppColors.primaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+
+        backgroundColor: AppColors.neatralColor,
+
+        leading: const Icon(
+          Icons.movie_outlined,
+          color: AppColors.primaryColor,
+        ),
+
+        actions: const [Icon(Icons.search, color: AppColors.primaryColor)],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image(
+                  image: AssetImage('images/1.png'),
+                  height: 600,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  colorBlendMode: BlendMode.darken,
+                ),
+                Positioned.fill(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          const Color.fromARGB(200, 20, 20, 20),
+                        ],
+                        stops: const [0.5, 1],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          'STRANGER THINGS',
+                          softWrap: true,
+                          textAlign: TextAlign.center,
+
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 44,
+                            fontWeight: FontWeight.bold,
+                            height: 0.9,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              'New Season',
+                              style: TextStyle(
+                                color: AppColors.primaryColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              '•',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              'TV Series',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              '•',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              'Sci-Fi',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 12,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: AppColors.secondaryColor,
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.play_arrow_outlined,
+                                      color: AppColors.neatralColor,
+                                    ),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      'Play',
+                                      style: TextStyle(
+                                        color: AppColors.neatralColor,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 16),
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 12,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.add_outlined,
+                                      color: AppColors.secondaryColor,
+                                    ),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      'My List',
+                                      style: TextStyle(
+                                        color: AppColors.secondaryColor,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text(
+                    'Continue Watching for You',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      5,
+                      (index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        width: 120,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                            image: AssetImage('images/${index + 1}.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Trending Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    height: 220,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+
+                      itemBuilder: (context, index) {
+                        return SizedBox(
+                          width: 210,
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Positioned(
+                                left: -5,
+                                bottom: 0,
+                                child: Text(
+                                  '${index + 1}',
+                                  style: TextStyle(
+                                    fontSize: 140,
+                                    fontWeight: FontWeight.bold,
+                                    shadows: [
+                                      Shadow(
+                                        color: AppColors.primaryColor
+                                            .withValues(alpha: 0.5),
+                                        blurRadius: 0,
+                                      ),
+                                    ],
+                                    foreground: Paint()
+                                      ..style = PaintingStyle.stroke
+                                      ..strokeWidth = 2
+                                      ..color = AppColors.primaryColor
+                                          .withValues(alpha: 0.5),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: 50,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(4),
+                                  child: Image.asset(
+                                    'images/${index + 1}.png',
+                                    width: 140,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'New Releases',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 160,
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) => SizedBox(width: 8),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        width: 120,
+                        child: Image.asset('images/${index + 1}.png'),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
